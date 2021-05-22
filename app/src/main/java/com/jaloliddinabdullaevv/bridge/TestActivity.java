f292abe6b2a3f65810d44b65d8daf1d61e68654c
Jaloliddin Abdullaev <jaloliddinabdullaev49@gmail.com>
2021-05-22 05:02:57 +0500

    Test Activity is Created and minor added design to cards

---
+++ app/src/main/java/com/jaloliddinabdullaevv/bridge/TestActivity.java
@@ -0,0 +1,44 @@
+package com.jaloliddinabdullaevv.bridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class TestActivity extends AppCompatActivity {
+    MaterialCardView firstStep, secondStep, thirdStep;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+        getSupportActionBar().setTitle("Savol & Test");
+        firstStep=findViewById(R.id.firstStep);
+        secondStep=findViewById(R.id.secondStep);
+        thirdStep=findViewById(R.id.thirdStep);
+
+        firstStep.setTranslationX(-1100);
+        firstStep.animate().translationXBy(1100).setDuration(1000);
+        secondStep.setTranslationX(1100);
+        secondStep.animate().translationXBy(-1100).setDuration(1000);
+        thirdStep.setTranslationX(-1100);
+        thirdStep.animate().translationXBy(1100).setDuration(1000);
+
+        firstStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        secondStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
